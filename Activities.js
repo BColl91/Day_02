0f14760020ab493368d85e734fa061641f2d0eab
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-02 18:57:10 +0100

    Save_01

--- Activities.js
+++ Activities.js
@@ -1,12 +1,10 @@
-// Act 1
-
+// Act 1//
 // let age = 16;
 // let serve = (age < 17) ? 'You arent old enough' : 'Yes I can serve you'
 // console.log(serve)
 
 
-//Act 2
-
+//Act 2//
 // let top = "Tuna";
 
 // switch(top){
@@ -27,17 +25,113 @@
 // }
 
 
-//Act 3
+//Act 3//
+// let password = "totallynotpassword"; 
 
-let password = "gimmie_your_credit_deets";
-
-if (password .length >= 8) {
-    console.log("Password Accepted")
-} else;
-    console.log("Your password is too short. Please try again.")
-
-    let pass = (>= 8) ? 'Password Accepted' : 'Your password is too short. Please try again.'
-    console.log(pass)
+// if (password.length < 8) {
+//     console.log("The password is too short. Please try again.");
+// } else {
+//     console.log("Password Accepted. Welcome.");
+// }
 
 
-//Act 4
+
+//Act 4//
+// let num = 20;
+
+// if (num % 3 === 0 && num % 5 === 0) {
+//     console.log("fizz buzz");
+// } else if (num % 3 === 0) {
+//     console.log("fizz");
+// } else if (num % 5 === 0) {
+//     console.log("buzz");
+// } else {
+//     console.log(num);
+// }
+
+
+//Act 5//
+// let num = 2073;
+
+// let numStr = num.toString();
+// let reversedStr = numStr.split('').reverse().join('');
+// if (numStr === reversedStr) {
+//     console.log(`${num} is a palindrome.`);
+// } else {
+//     console.log(`${num} is not a palindrome.`);
+// }
+
+
+// Act 6//
+// let time = 4;
+// let placeOfWork = "the office";
+// let townOfHome = "home";
+
+// if (time >= 0 && time < 6) {
+//     console.log(`At ${time}:00, I'm trying to sleep.`);
+// } else if (time >= 6 && time < 8) {
+//     console.log(`At ${time}:00, I'm at ${townOfHome}.`);
+// } else if (time >= 8 && time < 9) {
+//     console.log(`At ${time}:00, I'm commuting.`);
+// } else if (time >= 9 && time < 17) {
+//     console.log(`At ${time}:00, I'm at ${placeOfWork}.`);
+// } else if (time >= 17 && time < 18) {
+//     console.log(`At ${time}:00, I'm on my way home.`);
+// } else if (time >= 18 && time < 24) {
+//     console.log(`At ${time}:00, I'm at ${townOfHome}.`);
+// } else {
+//     console.log(`That's an invalid time.`);
+// }
+
+
+
+//Act 7//
+let str = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi";
+
+const vowels = "aeiouAEIOU";
+
+let lastVowelIndex = -1;
+
+for (let i = str.length - 1; i >= 0; i--) {
+    if (vowels.includes(str[i])) {
+        lastVowelIndex = i;
+        break;
+    }
+}
+
+console.log(`The index of the last vowel is: ${lastVowelIndex}`);
+
+
+//Act 8//
+// let word = "level";
+
+// let isSame = word.length > 0 && word[0] === word[word.length - 1];
+
+// console.log(isSame);
+
+// //****OR****
+// function isFirstAndLastSame(word) {
+//     return word.length > 0 && word[0] === word[word.length - 1];
+// }
+
+// console.log(isFirstAndLastSame("radar"));
+// console.log(isFirstAndLastSame("hello"));
+// console.log(isFirstAndLastSame("level"));
+// console.log(isFirstAndLastSame("world"));
+// console.log(isFirstAndLastSame(""));
+
+
+//Act 9//
+// let num1 = 4;
+// let num2 = 8;
+
+// let sum = num1 + num2;
+
+// let result;
+// if (sum % 2 === 0) {
+//     result = sum;
+// } else {
+//     result = num1 * num2;
+// }
+
+// console.log(result);
--- ArrayAct.js
+++ ArrayAct.js
@@ -1,17 +1,127 @@
-//Activity 1
+//Activity 1.1
+// let songs = [
+//     "Hounds to Harmatia - Poets of the Fall ",
+//     "Louder than Bombs - Bantan Boys ",
+//     "Home In Florence - Jesper Kyd",
+// ]
+// console.log(songs)
 
-let songs = [
-    "Hounds to Harmatia - Poets of the Fall ",
-    "Louder than Bombs - Bantan Boys ",
-    "Home In Florence - Jesper Kyd",
-]
-console.log(songs)
+// songs.push("Home - GD & TOP")
+// console.log(songs)
 
-songs.push("Home - GD & TOP")
-console.log(songs)
+// songs.pop()
+// console.log(songs)
 
-songs.pop()
-console.log(songs)
 
-//Activity 2
 
+//Activity 1.2
+// Define an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+// Use map() to create a new array with each number doubled
+let doubledNumbers = numbers.map(function(number) {
+    return number * 2;
+});
+
+// Log the original array
+console.log("Original numbers:", numbers);
+
+// Log the new array with doubled numbers
+console.log("Doubled numbers:", doubledNumbers);
+
+/////////or////////
+// Define an array of people objects
+let people = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 35 }
+];
+
+// Use map() to create a new array with just the names
+let names = people.map(function(person) {
+    return person.name;
+});
+
+// Log the original array of people
+console.log("People:", people);
+
+// Log the new array of names
+console.log("Names:", names);
+
+
+
+
+//Activity 2.1
+
+//Activity 2.2
+// Loop to generate and log 6 random numbers between 1 and 50
+for (let i = 0; i < 6; i++) {
+    // Generate a random number between 1 and 50
+    let randomNumber = Math.floor(Math.random() * 50) + 1;
+    
+    // Log the random number to the console
+    console.log(randomNumber);
+}
+//////OR///////////
+// Create an array to store the random numbers
+let randomNumbers = [];
+
+// Loop to generate 6 random numbers between 1 and 50
+for (let i = 0; i < 6; i++) {
+    // Generate a random number between 1 and 50
+    let randomNumber = Math.floor(Math.random() * 50) + 1;
+    
+    // Add the random number to the array
+    randomNumbers.push(randomNumber);
+}
+
+// Log the array of random numbers to the console
+console.log("Random Numbers:", randomNumbers);
+
+
+
+//Activity 2.3
+// Loop to count from 9 to 0
+for (let i = 9; i >= 0; i--) {
+    console.log(i);
+}
+//////OR///////////
+// Initialize the counter
+let i = 9;
+
+// Loop to count from 9 to 0
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
+//////OR///////////
+// Initialize the counter
+let i = 9;
+
+// Function to count down
+function countDown() {
+    console.log(i);
+    i--;
+    if (i < 0) {
+        clearInterval(counting);
+    }
+}
+
+// Set interval to call countDown every 1000 milliseconds (1 second)
+let counting = setInterval(countDown, 1000);
+
+
+
+
+
+//Activity 2.4
+///Display 4 films stored in an array. Use a for loop to show each film in the array. Use an if statement to check if the 3rd film in the array is Ghostbusters. If it is, return "Yay its Ghostbusters!". If it isn't return "Boo! We want Ghostbusters."
+
+//Activity 2.5
+///Generate a random number between 1-30 six times. For each random number generated, check if this number is divisible by 7 or not. Log out a message to the console if it is divisible by 7 or not.
+
+//Activity 2.6
+///Imagine you're a programmer for a social media platform. You have been tasked with building a prototype for a mutual followers program. Create 2 arrays of followers e.g. bexsFollowers & GeorgesFollowers. In these arrays place 4 names as strings. Make sure there are 2 names that are in BOTH arrays. Using a nested loop iterate over both arrays and console.log out the matching followers.
+
+//Activity 2.7
+//Research on do...while loop, find out about the difference between for loop, while loop and do...while loop. Give an examples of each. Waht are the pros and cons?
