2b92afcfa0532a77d11119336772baa2d035a28f
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-02 14:46:19 +0100

    save_02

---
+++ Activities.js
@@ -0,0 +1,43 @@
+// Act 1
+
+// let age = 16;
+// let serve = (age < 17) ? 'You arent old enough' : 'Yes I can serve you'
+// console.log(serve)
+
+
+//Act 2
+
+// let top = "Tuna";
+
+// switch(top){
+//     case "chicken":
+//     case "pepperoni":
+//         console.log("It sounds nice but I'm a pescetarian!");
+//     break;
+//     case "Cheese":
+//         console.log("Can't go wrong with cheese~!");
+//     break;
+//     case "Tuna":
+//         console.log("I need this topping on my pizza.");
+//     break;
+//     case "Pineapple":
+//         console.log("Ew.")
+//     default:
+//         console.log("I don't tihnk I want this on my pizza.")    
+// }
+
+
+//Act 3
+
+let password = "gimmie_your_credit_deets";
+
+if (password .length >= 8) {
+    console.log("Password Accepted")
+} else;
+    console.log("Your password is too short. Please try again.")
+
+    let pass = (>= 8) ? 'Password Accepted' : 'Your password is too short. Please try again.'
+    console.log(pass)
+
+
+//Act 4
---
+++ ArrayAct.js
@@ -0,0 +1,17 @@
+//Activity 1
+
+let songs = [
+    "Hounds to Harmatia - Poets of the Fall ",
+    "Louder than Bombs - Bantan Boys ",
+    "Home In Florence - Jesper Kyd",
+]
+console.log(songs)
+
+songs.push("Home - GD & TOP")
+console.log(songs)
+
+songs.pop()
+console.log(songs)
+
+//Activity 2
+
