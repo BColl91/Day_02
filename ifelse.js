d35bfc1f5dc84a24665d386bcb75075329489f4d
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-02 11:48:22 +0100

    save_01

---
+++ ifelse.js
@@ -0,0 +1,71 @@
+// if (1 === "1") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// if (1 != "1") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// let place = "Manc";
+// let weather = "Dull";
+
+// if (place == "Manc" && weather == "Sunny"){
+//     console.log("Check Again");
+// }
+// else if (place == "Manc" && weather == "Rain") {
+//     console.log("Obvs");
+// }
+// else if(place == "Manc" && weather == "Dull") {
+//     console.log("Guess it's Monday.")
+// }
+// else {
+//     console.log("what it isn't raining?");
+// }
+
+
+// let car = "Peugeot";
+
+// if(car == "Ford" || car == "GM"){
+//     console.log("You've got an American car!");
+// } else if(car == "Peugeot" || car == "Citroen"){
+//     console.log("You've got a French boy!");
+// } else if(car == "Honda" || car == "Toyota" || car == "Suzuki"){
+//     console.log("Japanese cars are dead quiet!");
+// } else if(car == "Mercedes"){
+//     console.log("You're a proper posh German!");
+// } else if(car == "Volkswagen"){
+//     console.log("German is good!");
+// } else if(car == "Hyundai" || car == "Kia"){
+//     console.log("South Korean car are getting popular!");
+// } else{ console.log("Your car is not in one of the top companies in the world.");
+// }
+
+// let car = "Peugeot";
+
+// switch(car){
+//     case "Ford":
+//     case "GM":
+//         console.log("You've got an American Car!");
+//         break;
+//     case "Peugeot":
+//     case "Citroen":
+//         console.log("You've got a French boy!");
+//         break;
+//     case "Honda":
+//     case "Toyota":
+//     case "Suzuki":
+//         console.log("Japanese cars are dead quiet!");
+//         break;
+//     case "Mecedes":
+//         console.log("You are a proper posh German!");
+//         break;
+//     case "Volkswagen":
+//         console.log("German is good!");
+//         break;
+//     default:
+//         console.log("Your car is not in the top ten companies in the world.")    
+// }
