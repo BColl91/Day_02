ea95de116beaa6f61dc4d75ff5e3d70a5bf0093f
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-04 16:16:35 +0100

    needs tuning

--- ArrayAct.js
+++ ArrayAct.js
@@ -59,11 +59,11 @@
 
 
 // // //Activity 2.4
-// function displayFilms(films) {
-//     for (let i = 0; i < films.length; i++) {
-//       console.log(films[i]);
+// const displayFilms = (filmList) => {
+//     for (let i = 0; i < filmList.length; i++) {
+//       console.log(filmList[i]);
 //     }
-//      if (films[2] === "Ghostbusters") {
+//      if (filmsList[2] === "Ghostbusters") {
 //       return "Yay it's Ghostbusters!";
 //     } else {
 //       return "Boo! We want Ghostbusters.";
@@ -72,10 +72,7 @@
 
 //   const filmsList = ["Spiderman Homecoming", "Cloud Atlas", "Ghostbusters", "John Wick"];
 //   console.log(displayFilms(filmsList));
-//   console.log("")
-//   const filmsList2 = ["Spiderman Homecoming", "Cloud Atlas", "What We Do in the Shadows", "John Wick"];
-//   console.log(displayFilms(filmsList2));
-  
+
 
 
 // // //Activity 2.5
@@ -107,25 +104,25 @@
 // }
 
 
-// //Activity 2.7
-//DO..WHILE LOOP//
-let i = 0;
-do {
-  console.log(`Iteration number ${i}`);
-  i++;
-} while (i < 5);
-// // Do..While Loop is useful at it will ensure to loop at least once regardless of its condition, but if said condition is meant to prevent any execution- this can lead to errors. Do..While loops are less commonly used. So others might not understand the code.//
+// // //Activity 2.7
+// //DO..WHILE LOOP//
+// let i = 0;
+// do {
+//   console.log(`Iteration number ${i}`);
+//   i++;
+// } while (i < 5);
+// // // Do..While Loop is useful at it will ensure to loop at least once regardless of its condition, but if said condition is meant to prevent any execution- this can lead to errors. Do..While loops are less commonly used. So others might not understand the code.//
 
-//WHILE LOOP//
-let i = 0;
-while (i < 5) {
-  console.log(`Iteration number ${i}`);
-  i++;
-}
-// //While loops are good for scenarios where the number of iterations is not known before hand and can potentially result in fewer iterations if the condition becomes fals early. The cons are that if the condition is never false, it can become an infinate loop.//
+// //WHILE LOOP//
+// let i = 0;
+// while (i < 5) {
+//   console.log(`Iteration number ${i}`);
+//   i++;
+// }
+// // //While loops are good for scenarios where the number of iterations is not known before hand and can potentially result in fewer iterations if the condition becomes fals early. The cons are that if the condition is never false, it can become an infinate loop.//
 
-//FOR LOOP//
-for (let i = 0; i < 5; i++) {
-    console.log(`Iteration number ${i}`);
-  }
-//   //For loops are clear and concise and can be easier to understand, as initialization, condition and increment/decrement are all in one place. This method is ideal for iterating over arrays. For loops can unfortunately be less readable when the loop logic is complex and is not suitable for situations where the number of iterations is not known beforehand.//
+// //FOR LOOP//
+// for (let i = 0; i < 5; i++) {
+//     console.log(`Iteration number ${i}`);
+//   }
+// //   //For loops are clear and concise and can be easier to understand, as initialization, condition and increment/decrement are all in one place. This method is ideal for iterating over arrays. For loops can unfortunately be less readable when the loop logic is complex and is not suitable for situations where the number of iterations is not known beforehand.//
